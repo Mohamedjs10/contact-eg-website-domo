f3a2455be8345c1840bb24228008721d07d26d65
Ahmed-Behery <ahmed.abdelmaboud@contact.eg>
2023-03-22 11:09:44 +0200

    about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,11 +1,16 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Typography, Button } from "@mui/material";
 import { colors } from "../../utils/const";
-
+//
+import { styles } from "../../components/secondary_navbar";
 //
 import AboutSection from "../../components/aboutSec/about";
 import AboutCardVal from "../../components/aboutCard/aboutCard";
 import Statistics from "../../components/statistics/statistics";
+import Carousal from "../../components/VerticalCarousel";
+import MemberItem from "../../components/MemberCard";
+
+//
 import { useRouter } from "next/router";
 import en from "../../../locales/en";
 import ar from "../../../locales/ar";
@@ -14,6 +19,13 @@ export default function About() {
   const router = useRouter();
   const { locale } = router;
   const t = locale === "en" ? en : ar;
+
+  const [active, setActive] = useState("1");
+
+  const handleActiveChange = (idx) => {
+    setActive(idx);
+  };
+
   return (
     <Box>
       <AboutSection />
@@ -103,6 +115,76 @@ export default function About() {
             icon="/images/value.png"
             heading={t.about.card.valueTitle}
             textArr={t.about.card.valueText}
+          />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          my: {
+            xs: 6,
+            md: 12,
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: {
+              xs: "center",
+              md: "space-between",
+            },
+            alignItems: "center",
+            flexDirection: {
+              xs: "column",
+              md: "row",
+            },
+            mx: 12,
+          }}
+        >
+          <Box
+            sx={{
+              textAlign: {
+                xs: "center",
+                md: locale === "en" ? "left" : "right",
+              },
+            }}
+          >
+            <Typography variant="h5">مجلس الإدارة وفريق العمل</Typography>
+            <Typography sx={{ mt: 1, color: "#667085" }} variant="subtitle2">
+              نحن نبحث دائمًا عن الأفراد المتحمسين والديناميكيين والموهوبين.
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              mt: {
+                xs: 2,
+                md: 0,
+              },
+            }}
+          >
+            <Button
+              onClick={() => handleActiveChange("1")}
+              sx={active == "1" ? styles.downloadBtn : styles.loginBtn}
+              variant="outlined"
+            >
+              مجلس الادارة
+            </Button>
+            <Button
+              className="mx-3"
+              onClick={() => handleActiveChange("2")}
+              sx={active === "2" ? styles.downloadBtn : styles.loginBtn}
+            >
+              الإدارة التنفيذية
+            </Button>
+          </Box>
+        </Box>
+        <Box>
+          <Carousal
+            Component={MemberItem}
+            slidesPerView={5.5}
+            itemsArray={
+              active === "1" ? t.general.directors : t.general.mangers
+            }
           />
         </Box>
       </Box>
