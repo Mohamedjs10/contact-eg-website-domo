a9d70cefadfc31fbcbf34c9980bc3e8577468dde
Ahmed-Behery <ahmed.abdelmaboud@contact.eg>
2023-07-10 16:40:34 +0200

    map label and handle search

--- src/components/maps/maps.js
+++ src/components/maps/maps.js
@@ -151,7 +151,6 @@ export default function MapBox() {
               },
             }}
             renderInput={(params) => {
-              console.log(params);
               return (
                 <TextField
                   placeholder={t.map.placeholderBranch}
